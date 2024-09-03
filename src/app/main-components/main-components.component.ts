52f43d3a4296c430e1aee5eba20884648686f065
Maiki <ismenemaiki@gmail.com>
2024-09-03 15:06:15 -0300

    feature: feito header da aplicação

--- src/app/main-components/main-components.component.ts
+++ src/app/main-components/main-components.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'it-main-components',
-    templateUrl: './main-components.component.html'
+    templateUrl: './main-components.component.html',
+    styleUrls: ['./main-components.component.scss']
 })
 export class MainComponentsComponent implements OnInit {
     ngOnInit() {}
