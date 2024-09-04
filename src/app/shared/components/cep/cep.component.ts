b7892a2cbb9a98ae41fd800d336867913513804d
Maiki <ismenemaiki@gmail.com>
2024-09-03 23:30:34 -0300

    feature: criado componente de cep

--- src/app/shared/components/cep/cep.component.ts
+++ src/app/shared/components/cep/cep.component.ts
@@ -1,17 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { CepService } from '../../services/cep.service';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
 
 @Component({
   selector: 'it-cep',
   templateUrl: './cep.component.html',
   styleUrls: ['./cep.component.scss'],
 })
-export class CepComponent implements OnInit {
-  constructor(private cepService: CepService) {}
+export class CepComponent implements OnInit, OnChanges {
+  @Input() cep: string = '';
+  @Output() cepChanged = new EventEmitter<string>();
+  formGroup!: FormGroup;
+  constructor(private cepService: CepService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.cepService.searchAdressByCep('04850180').then((res) => {
-      console.log('get cep', res);
+    this.formGroup = this.fb.group({
+      cep: new FormControl(),
     });
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['cep'] && this.cep) {
+      this.searchCep()
+      this.formGroup.get('cep')?.setValue(this.cep, { emitEvent: false });
+    }
+  }
+  searchCep(): void {
+    const cepValue = this.formGroup.get('cep')?.value;
+    if (cepValue) {
+      this.cepService.searchAdressByCep(cepValue).then((res) => {
+        console.log('get cep', res);
+        this.cepChanged.emit(res);
+      });
+    }
   }
 }
