d56f2c749566c3795d0ad4a8eec408096f69e0bd
Maiki <ismenemaiki@gmail.com>
2024-09-04 22:22:12 -0300

    fix: removido funcao inutilizada

--- src/app/shared/functions/filter.function.ts
+++
@@ -1,9 +0,0 @@
-// FUNÇÃO GENERICA PARA FILTRO DE LISTA
-export function filterList(list: Array<any>, filters: Array<string>): Array<Object> {
-    return list.filter((item) => {
-      return (filters.every((filter) =>
-        Object.keys(filter).every((key: any) => filter[key] === item[key]))
-      );
-    });
-  }
-  
